fdcf455101c8ea55bb7051a9d9debe5c65a74bcf
Aman Naredi <81749870+amannaredi@users.noreply.github.com>
2024-07-22 15:13:05 +0100

    Time: 129 ms (43.92%), Space: 18.2 MB (15.61%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,13 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count = nums.count(0)
+        # count = nums.count(0)
 
-        for _ in range(count):
-            nums.remove(0)
+        # for _ in range(count):
+        #     nums.remove(0)
             
-        for _ in range(count):
+        # for _ in range(count):
             
-            nums.append(0)
+        #     nums.append(0)
+        left = 0
+
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[right], nums[left] = nums[left], nums[right]
+                left += 1
+        
+        return nums
+            
+
 
 
