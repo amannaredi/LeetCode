9efd089b2edab5fdcab407744879c60207342d2f
Aman Naredi <81749870+amannaredi@users.noreply.github.com>
2024-07-23 21:55:46 +0100

    Time: 50 ms (91.98%), Space: 17.8 MB (35.33%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         l = 0
-        
         profit = 0
         for r in range(1,len(prices)):
             max_profit = 0
@@ -10,4 +9,5 @@ class Solution:
             max_profit = max(max_profit,prices[r]-prices[l])
             l = r
             profit+=max_profit
+            
         return profit
