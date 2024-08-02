b0d9ccbb3a72673960def37c4c90070801942983
Aman Naredi <81749870+amannaredi@users.noreply.github.com>
2024-08-02 23:37:39 +0100

    Time: 496 ms (18.61%), Space: 18.2 MB (7.26%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -11,17 +11,4 @@ class Solution:
             
             run -=1
         return True
-    #    if len(nums) == 1:
-    #     return True 
-    #    for i in range(nums[0]):
-    #     if i == len(nums)-1:
-    #         return False
-    #     for j in range(1,nums[i]+1):
-    #         if i + 1 + j == len(nums):
-    #             print(i)
-    #             return True
-        
-    #     continue
-    
-    #     return False
-            
+       
