0d724ec0456a7fa1b6b17589d1db8ca0dbfd7d1d
Aman Naredi <81749870+amannaredi@users.noreply.github.com>
2025-03-03 00:16:51 +0000

    Time: 0 ms (100%), Space: 18.1 MB (67.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,62 +1,31 @@
-# class Solution:
-#     def search(self, nums: List[int], target: int) -> int:
-#         n = len(nums)
-#         left = 0
-#         right = n-1
-
-#         while left <= right:
-#             mid = (left+right)//2
-#             if nums[mid] > nums[-1]:
-#                 left = mid + 1
-#             right = mid - 1
-        
-#         def binarySearch(leftVal, rightVal,target):
-#             left, right = leftVal, rightVal
-#             while left <= right:
-#                 mid = (left + right)//2
-#                 if nums[mid] == target:
-#                     return mid
-#                 elif nums[mid] > target:
-#                     right = mid - 1
-#                 else:
-#                     left = mid + 1
-            
-#             return -1
-        
-#         if binarySearch(0,left-1,target) != -1:
-#             return binarySearch(0,left-1,target)
-        
-#         return binarySearch(left,n-1,target)
-
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        left, right = 0, n - 1
+        left = 0
+        right = n-1
 
-        # Find the index of the pivot element (the smallest element)
         while left <= right:
-            mid = (left + right) // 2
+            mid = (left+right)//2
             if nums[mid] > nums[-1]:
                 left = mid + 1
             else:
                 right = mid - 1
-
-        # Binary search over an inclusive range [left_boundary ~ right_boundary]
-        def binarySearch(left_boundary, right_boundary, target):
-            left, right = left_boundary, right_boundary
+        
+        def binarySearch(left, right,target):
             while left <= right:
-                mid = (left + right) // 2
+                mid = (left + right)//2
                 if nums[mid] == target:
                     return mid
                 elif nums[mid] > target:
                     right = mid - 1
                 else:
                     left = mid + 1
+            
             return -1
+        
+        if binarySearch(0,left-1,target) != -1:
+            return binarySearch(0,left-1,target)
+        
+        return binarySearch(left,n-1,target)
 
-        # Binary search over elements on the pivot element's left
-        if (answer := binarySearch(0, left - 1, target)) != -1:
-            return answer
 
-        # Binary search over elements on the pivot element's right
-        return binarySearch(left, n - 1, target)
