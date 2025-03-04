5c253777edd2a54b77777da4f2e5b7c6f2ee3b65
Aman Naredi <81749870+amannaredi@users.noreply.github.com>
2025-03-04 12:31:20 +0000

    Time: 41 ms (31.58%), Space: 18.1 MB (99.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -2,12 +2,9 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         run = 0
         for num in nums:
-            print(run, num)
             if run < 0:
                 return False
-
             run = max(run, num)
-            
             run -=1
         return True
        
